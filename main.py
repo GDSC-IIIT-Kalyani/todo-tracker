c949ac9a98de674ff140f51dbe1896a05175d4ed
Tathagata <tathagata.roy1278@gmail.com>
2021-10-04 14:37:59 +0530

    Removed folder selection and mandatorily create directory todo

--- main.py
+++ main.py
@@ -23,20 +23,15 @@ def run(command):
         print("This command doesnt exist, try these : \n"+"\n".join(commands.keys()))
         return 1;
 
-print("Already existsing folders : \n"+"\n".join(os.listdir(root)))
-folder = input("Folder : ")
-root+=folder
+root+='todo'
 
 if folder == 'todo':
     is_todo = 1
 
-ans = "y"
 if not os.path.exists(root):
-    ans = input("This folder does not exist, are you sure you want to create a new folder?(y/n) : ")
-    if(ans == "y"):
-        os.makedirs(root)
+    os.makedirs(root)
 
-while(True and ans == "y"):
+while(True):
     command = input("#")
     if run(command)==0 :
         break

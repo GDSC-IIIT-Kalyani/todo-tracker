81f11d64025c01d7b119782272eeb0fb7ab1102d
Tathagata <tathagata.roy1278@gmail.com>
2021-10-11 12:47:18 +0530

    Changed run.sh to just open a terminal

---
+++ open_terminal.sh
@@ -0,0 +1,1 @@
+gnome-terminal --working-directory=/home/tatan/Desktop/Tests/todo_tracker --geometry=50x20 -x bash -c "./run.sh"
--- run.sh
+++
@@ -1,1 +0,0 @@
-gnome-terminal --working-directory=/home/tatan/Desktop/Tests/todo_tracker --geometry=50x20 -x bash -c "./main.py"
